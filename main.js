c399c8ff628e01bf390bf59b30cdc3c3524cb3f7
Gskds <weddeveloper460@gmail.com>
2025-11-08 11:13:36 +0300

    update

---
+++ main.js
@@ -0,0 +1,8 @@
+const socialLinks = document.querySelector(".social-links");
+const share = document.querySelector(".share-icon")
+
+
+share.addEventListener("click", function 
+() {
+  socialLinks.classList.toggle('active');
+});
