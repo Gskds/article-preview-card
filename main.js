ebfa9fc5f53ad9544c5f30824b6a224e0e0b979d
Gskds <weddeveloper460@gmail.com>
2025-11-09 17:01:31 +0300

    update main.js

--- main.js
+++ main.js
@@ -3,9 +3,14 @@ const share = document.querySelector(".share")
 const shareIcon = document.querySelector(".share-icon")
 
 
-share.addEventListener("click", function 
-() {
+share.addEventListener("click", function() {
   socialLinks.classList.toggle('active');
-  share.style.backgroundColor = 'hsl(214, 17%, 51%)';
-  shareIcon.querySelector('path').style.fill = 'white'
+  
+  if (socialLinks.classList.contains('active')) {
+    share.style.backgroundColor = 'hsl(214, 17%, 51%)';
+    shareIcon.querySelector('path').style.fill = 'white'
+  } else {
+    share.style.backgroundColor = 'hsl(210, 46%, 95%)';
+    shareIcon.querySelector('path').style.fill = '#6E8098'
+  }
 });
